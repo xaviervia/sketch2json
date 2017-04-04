1e9ba67e50353b829e134c5f20ee86c0214094e4
Xavier Via <fernando.via@gmail.com>
2017-04-04 23:38:58 +0200

    Add the missing tests to the list

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,5 @@
 const getZipContentsTests = require('./getZipContentsTests')
+const getImageFromZipTests = require('./getImageFromZipTests')
 const getJSONFromZipTests = require('./getJSONFromZipTests')
 const getSketchContentsTests = require('./getSketchContentsTests')
 const sketch2jsonTests = require('./sketch2jsonTests')
@@ -11,6 +12,7 @@ const withNamespace = namespace => ({description, test, shouldEqual}) => ({
 
 module.exports = []
   .concat(getZipContentsTests.map(withNamespace('getZipContents')))
+  .concat(getImageFromZipTests.map(withNamespace('getImageFromZip')))
   .concat(getJSONFromZipTests.map(withNamespace('getJSONFromZip')))
   .concat(getSketchContentsTests.map(withNamespace('getSketchContents')))
   .concat(sketch2jsonTests.map(withNamespace('sketch2json')))
